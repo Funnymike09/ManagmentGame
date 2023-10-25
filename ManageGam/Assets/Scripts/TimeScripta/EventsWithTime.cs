2bf8b2f9090050dfca2e358e3618ed4c74d63910
Funnymike09 <mike.nip@mail.ru>
2023-10-25 05:53:00 +0500

    Theme done and ready for tweaking ,UI elements for visual

--- ManageGam/Assets/Scripts/TimeScripta/EventsWithTime.cs
+++ ManageGam/Assets/Scripts/TimeScripta/EventsWithTime.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using XCharts;
+using XCharts.Runtime;
 
 public class EventsWithTime : MonoBehaviour
 {
+    
     public Button Mail;
     private void OnEnable()
     {
@@ -23,6 +26,14 @@ public class EventsWithTime : MonoBehaviour
     {
         if (TimeManager.Hour == 10 && TimeManager.Minute == 5) 
             Mail.interactable = true;
+            
+
     }
-    
+
+    public LineChart lineChart;
+
+    void bruh()
+    {
+        
+    }
 }
