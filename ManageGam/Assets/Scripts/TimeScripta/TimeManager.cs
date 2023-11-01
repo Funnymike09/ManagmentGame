e8d758c3d2f2a7df4848298cc1657f7eb335377e
Funnymike09 <mike.nip@mail.ru>
2023-11-02 02:39:42 +0500

    Got rid of old code

--- ManageGam/Assets/Scripts/TimeScripta/TimeManager.cs
+++ ManageGam/Assets/Scripts/TimeScripta/TimeManager.cs
@@ -49,8 +49,8 @@ public class TimeManager : MonoBehaviour
 
     void Start()
     {
-        Minute = 00;
-        Hour = 9;
+        Minute = 30;
+        Hour = 10;
         timer = minuteToRealTIme;
         OnHourChanged += PriceChange;
         OnHourChanged += AddGraphData;
