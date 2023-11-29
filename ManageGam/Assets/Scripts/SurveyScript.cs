62b3b2d95ec23fe8425fb85489530f645a36bad9
DaraghFiorilla <daraghfiorillafpl@gmail.com>
2023-11-29 12:38:45 +0000

    bruh

---
+++ ManageGam/Assets/Scripts/SurveyScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurveyScript : MonoBehaviour
+{
+    private TimeManager timeManager;
+
+    private void Start()
+    {
+        timeManager = GetComponent<TimeManager>();
+    }
+
+    public void OnClick()
+    {
+        Application.OpenURL("https://forms.office.com/e/8SmwKueK43");
+    }
+}
